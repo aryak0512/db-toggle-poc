1ac2b49a18f8974577530e09b27174985f652a7e
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-03-23 10:13:51 +0530

    Initial project setup

---
+++ src/main/java/com/aryak/db/CacheToSqlApplication.java
@@ -0,0 +1,13 @@
+package com.aryak.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheToSqlApplication.class, args);
+	}
+
+}
